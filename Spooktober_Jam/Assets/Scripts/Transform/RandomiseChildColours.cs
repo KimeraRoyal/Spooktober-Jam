a0281a71370209f1bb692d3f8e146ce5ec5242b1
SparkleKimera <kai.anderson.ellis.brown@gmail.com>
2021-10-30 21:06:24 +0100

    Random colours for People.

---
+++ Spooktober_Jam/Assets/Scripts/Transform/RandomiseChildColours.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Spooktober
+{
+    public class RandomiseChildColours : MonoBehaviour
+    {
+        private SpriteRenderer[] m_spriteRenderers;
+
+        [SerializeField] private Gradient m_gradient;
+
+        private void Awake()
+        {
+            m_spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+
+            var colour = m_gradient.Evaluate(Random.Range(0.0f, 1.0f));
+            foreach (var spriteRenderer in m_spriteRenderers)
+            {
+                spriteRenderer.color = colour;
+            }
+        }
+    }
+}

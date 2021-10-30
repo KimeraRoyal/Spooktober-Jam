0256b44d80258a313d41672a37303a1cf450ca5b
SparkleKimera <kai.anderson.ellis.brown@gmail.com>
2021-10-30 22:25:02 +0100

    Added ability for camera to follow target.

---
+++ Spooktober_Jam/Assets/Scripts/Visuals/LerpPositionToTarget.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+namespace Spooktober.Visuals
+{
+    public class LerpPositionToTarget : MonoBehaviour
+    {
+        [SerializeField] private Transform m_target;
+
+        [SerializeField] private Vector3 m_followAmount;
+        [SerializeField] private float m_smoothTime;
+
+        private Vector3 m_startingPosition;
+        
+        private Vector3 m_velocity;
+
+        public Transform Target
+        {
+            get => m_target;
+            set => m_target = value;
+        }
+
+        private void Awake()
+        {
+            m_startingPosition = transform.position;
+        }
+
+        private void Update()
+        {
+            Vector3 targetPosition;
+            if (!m_target) { targetPosition = m_startingPosition; }
+            else
+            {
+                targetPosition = new Vector3();
+                for (var i = 0; i < 3; i++)
+                {
+                    targetPosition[i] = Mathf.Lerp(m_startingPosition[i], m_target.position[i], m_followAmount[i]);
+                }
+            }
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref m_velocity, m_smoothTime);
+        }
+    }
+}
